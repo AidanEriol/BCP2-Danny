78e61cea7bc148825b8bc1d3668c8fd1f9952465
Danny Danger <dannywithachance@gmail.com>
2018-05-10 19:02:07 -0700

    added links, CSS changes

--- scripts/main.js
+++ scripts/main.js
@@ -7,9 +7,3 @@ function Projects(rawDataObject) {
   this.projectInfo = rawDataObject.projectInfo;
   this.finishedDate = rawDataObject.finishedDate;
 }
-
-Projects.prototype.toHtml = function() {
-  var $template = $('template').html();
-  var compile = Handlebars.compile($template);
-  $('main').append(compile(this));
-}
