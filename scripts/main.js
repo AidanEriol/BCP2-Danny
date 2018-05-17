eef379f0f35777338ba15a34acba63a500427da0
Danny Danger <dannywithachance@gmail.com>
2018-05-16 20:38:07 -0700

    added jquery

--- scripts/main.js
+++ scripts/main.js
@@ -8,3 +8,23 @@ var projects = [];
   this.projectPhoto = projectPhoto;
   this.finishedDate = finishedDate;
 }
+
+newProject.prototype.toHtml = function() {
+  var $addProject = $('.template').clone();
+  $newProject.removeClass('template');
+  $newProject.find('h1').html(this.projectName);
+  $newProject.find('p').html(this.projectInfo);
+  $newProject.find('.projectPhoto').attr('src', this.projectPhoto);
+  $newProject.find('time').attr('pubdate', this.finishedDate);
+  $newArticle.find('time').html('about ' + parseInt((new Date() - new Date(this.finishedDate)) / 60 / 60 / 24 / 1000) + ' days ago');
+  $newArticle.append('<hr>');
+  return $addProject;
+}
+
+completedProjects.forEach(function(projectObject) {
+  projects.push(new newProject(projectObject));
+});
+
+projects.forEach(function(project) {
+  $('#projects').append(project.toHtml());
+});
